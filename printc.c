dcfbc8b97e9e3a32310c1250d053cf7517e34f5d
Nataly-Aketch <nataliebeckie@gmail.com>
2023-01-25 23:18:37 +0300

    Printf

--- printc.c
+++ printc.c
@@ -7,16 +7,7 @@
 int printc(va_list list)
 {
 	char c = va_arg(list, int);
-<<<<<<< HEAD
-	putchar(c);
-	return (1);
-=======
 
 	putchar(c);
-<<<<<<< HEAD
-	return (0);
->>>>>>> 9c25686dbbc8d326b3cfe989789994b8081180f0
-=======
 	return (1);
->>>>>>> 602bce648a895f25346b68708612353427051e31
 }
