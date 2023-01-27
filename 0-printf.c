1137984ceecd8cb114adb2fee70b1cd409649be0
Nataly-Aketch <nataliebeckie@gmail.com>
2023-01-27 21:27:37 +0300

    Printf

--- 0-printf.c
+++ 0-printf.c
@@ -37,11 +37,6 @@ int _printf(const char *format, ...)
 
 			}
 		}
-		/*else if (format[i] == '%' && format[i++] == '%')
-		{
-			putchar('%');
-			length++;
-		}*/
 		else
 		{
 			putchar(format[i]);
