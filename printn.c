597e3313ec388bfddf7ec9ed627988cde7f6dfd0
Nataly-Aketch <nataliebeckie@gmail.com>
2023-01-28 21:05:25 +0300

    Print number

--- printn.c
+++ printn.c
@@ -1,38 +1,30 @@
 #include "main.h"
 /**
- * printn - prints integer
+ * printn - prints an int
  * @list: variable list
- * Return: printed number
+ * Return: length of variable
  */
 int printn(va_list list)
 {
-	int n = va_arg(list, int), j, i = 0;
+	long int n = va_arg(list, long int), j;
+	int i = 0;
 
 	if (n < 0)
 	{
 		putchar('-');
 		n = -n;
+	}
+	j = 0;
+	while (n > 0)
+	{
+		j = j * 10 + n % 10;
+		n /= 10;
 		i++;
 	}
-	if (n <= 9)
+	while (j > 0)
 	{
-		putchar(n + '0');
-		i++;
-	}
-	else
-	{
-		j = 0;
-		while (n > 0)
-		{
-			j = j * 10 + (n % 10);
-			n /= 10;
-		}
-		while (j > 0)
-		{
-			putchar(j % 10 + '0');
-			i++;
-			j /= 10;
-		}
+		putchar(j + '0');
+		j /= 10;
 	}
 	return (i);
 }
