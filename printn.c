87850402d10f6edca57f7ee8f4e8c1ab53d8a798
Nataly-Aketch <nataliebeckie@gmail.com>
2023-01-29 14:56:39 +0300

    Print number

--- printn.c
+++ printn.c
@@ -8,32 +8,33 @@ int printn(va_list list)
 {
 	int n = va_arg(list, int), i = 0;
 	long int j;
+	int last;
 
 	if (n < 0)
 	{
 		putchar('-');
-		n = -n;
-		i++;
-	}
-	if (n <= 9)
-	{
-		putchar(n + '0');
+		last = -(n % 10);
+		n /= -10;
 		i++;
 	}
 	else
 	{
-		j = 0;
-		while (n > 0)
-		{
-			j = j * 10 + (n % 10);
-			n /= 10;
-		}
-		while (j > 0)
-		{
-			putchar(j % 10 + '0');
-			i++;
-			j /= 10;
-		}
+		last = n % 10;
+		n /= 10;
 	}
+	j = 0;
+	while (n > 0)
+	{
+		j = j * 10 + (n % 10);
+		n /= 10;
+	}
+	while (j > 0)
+	{
+		putchar(j % 10 + '0');
+		i++;
+		j /= 10;
+	}
+	putchar(last + '0');
+	i++;
 	return (i);
 }
